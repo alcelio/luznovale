876194e13b3cbfdaf1285d547418721507608cb9
Alcélio Gomes <doalcelio@gmail.com>
2017-06-16 00:42:20 -0300

    Alteração para ajustar o pacote de excessoes

--- luznovale-web/src/main/java/br/com/luznovale/exceptions/ScrenException.java
+++
@@ -1,9 +0,0 @@
-package br.com.luznovale.exceptions;
-
-public class ScrenException extends Throwable{
-	private static final long serialVersionUID = 1L;
-
-	public ScrenException(String mensagem) {
-		super(mensagem);
-	}
-}
